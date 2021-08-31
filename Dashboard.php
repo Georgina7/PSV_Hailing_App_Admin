e05e46906f2630a698042109f126c9d13122edf7
ShemMwanza <shem.mwanza@strathmore.edu>
2021-08-31 11:59:35 +0300

    final changes

--- Dashboard.php
+++ Dashboard.php
@@ -111,13 +111,13 @@ if($value['status']== "disabled"){
                     <a href="Routes.php" target="frame" class="text-xl cursor-pointer text-gray-50 hover:text-blue-600 hover:opacity-75">ROUTES</a>
                 </div>
 
-                <div class="box-content flex p-2 pl-4 m-2 my-1 space-x-3">
+                <!-- <div class="box-content flex p-2 pl-4 m-2 my-1 space-x-3">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 stroke-current text-gray-50" fill="none" class="bi bi-file-earmark-check" viewBox="0 0 16 16">
                       <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"/>
                       <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
                     </svg>
                     <a href="Approvals.php" target="frame" class="text-xl cursor-pointer text-gray-50 hover:text-blue-600 hover:opacity-75">APPROVALS</a>
-                </div>
+                </div> -->
 
                 
             </div>
